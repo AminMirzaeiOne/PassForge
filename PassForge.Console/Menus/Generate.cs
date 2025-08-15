d6c1fdc2948c793eed0a8e6676c4579be7ec3f08
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-16 03:01:49 +0330

    - Update Generate Class Base Code

--- PassForge.Console/Menus/Generate.cs
+++ PassForge.Console/Menus/Generate.cs
@@ -1,13 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PassForge.Core.Models;
+using PassForge.Core.Services;
+using PassForge.Console.Services;
+using PassForge.Console.Views;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PassForge.Console.Menus
 {
-    public class Generate
+    public static class Generate
     {
+        public static void Show(PasswordOptions options)
+        {
+            ConsoleUI.WriteTitle("تولید پسورد");
 
+            var user = new UserInfo
+            {
+                FirstName = UserInput.Prompt("نام"),
+                LastName = UserInput.Prompt("نام خانوادگی"),
+                Nickname = UserInput.Prompt("نام مستعار", true),
+                Email = UserInput.Prompt("ایمیل", true, "مثال: a@b.com"),
+                Phone = UserInput.Prompt("شماره موبایل", true)
+            };
+
+            if (UserInput.PromptYesNo("تاریخ تولد را وارد می‌کنید؟", false))
+            {
+                var y = UserInput.PromptInt("سال تولد", 1900, DateTime.Now.Year);
+                var m = UserInput.PromptInt("ماه تولد", 1, 12);
+                var d = UserInput.PromptInt("روز تولد", 1, 31);
+                user.DateOfBirth = new DateTime(y, m, d);
+            }
+            else
+            {
+                user.AgeYears = UserInput.PromptInt("سن (سال)", 1, 120, 25);
+            }
+
+            var extra = UserInput.Prompt("کلمات اضافی (با کاما جدا کنید)", true);
+            if (!string.IsNullOrWhiteSpace(extra))
+                user.ExtraWords = extra.Split(',').Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+
+            // تولید
+            var gen = new PasswordGenerator(options);
+            var list = gen.Generate(user).ToList();
+
+            // انتخاب پیش‌نمایش
+            var previewCount = UserInput.PromptInt("تعداد موارد پیش‌نمایش", 0, 200, 20);
+            if (previewCount > 0) PasswordPreview.Show(list, previewCount);
+
+            // ذخیره
+            if (UserInput.PromptYesNo("ذخیره در فایل؟", true))
+            {
+                var path = UserInput.Prompt("مسیر فایل خروجی", false, "مثلاً output.txt");
+                FileExport.ExportToFile(list, path);
+            }
+        }
     }
 }
