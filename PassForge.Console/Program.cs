f3d7f826f506149a477b66155b810ad8195681ce
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-16 02:51:30 +0330

    - Update Program.cs Class PassForge.Console Project

--- PassForge.Console/Program.cs
+++ PassForge.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using PassForge.Console.Services;
 
 namespace PassForge.Console
 {
@@ -6,7 +6,20 @@ namespace PassForge.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // حالت سریع (بدون منو) اگر آرگومان داده شده باشد
+            if (args is { Length: > 0 })
+            {
+                if (PassForge.Console.Services.Args.TryHandleFastMode(args, out var exitCode))
+                {
+                    Environment.Exit(exitCode);
+                    return;
+                }
+            }
+
+            // حالت منویی و تعاملی
+            PassForge.Console.Menus.Main.Show();
         }
     }
 }
